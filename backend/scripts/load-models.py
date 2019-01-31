bccaab99e930347e92c7755a874ec800825e09bf
Jake Carlson <jrcarlson@smu.edu>
2019-01-31 08:26:36 -0600

    Fixed model loading

--- backend/scripts/load-models.py
+++ backend/scripts/load-models.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 
-from keras.models import load_model
+from keras.models import load_model, Model
+from keras.layers import Input
+
+from transform import TransformNet
 
 import keras_contrib
 import os
@@ -14,7 +17,10 @@ models = [MODELS_DIR + m for m in os.listdir(MODELS_DIR)]
 
 for m in models:
     # load model with keras
-    model = load_model(m)
+    inputs = Input(shape=(720, 720, 3))
+    transform_net = TransformNet(inputs, [32, 64, 128], 5)
+    model = Model(inputs=inputs, outputs=transform_net)
+    model.load_weights(m)
 
     # pickle to string
     serialized = pickle.dumps(model)
